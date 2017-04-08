24ea226dcac9a883f2909401573fe741fff7f228
cityhunter14 <cityhunter14@gmail.com>
2017-04-08 18:16:18 +0800

    Update limit.py

--- limit.py
+++ limit.py
@@ -5,12 +5,12 @@ url_limit = 'http://192.243.116.174:4567/assets/limit.txt'
 req = urllib.request.Request(url_limit)
 
 def getLimitTime():
-    limitTime=1800
+    limitTime=2700
     try:
         with urllib.request.urlopen(req, timeout=20) as response:
             limitTime = int(response.readline().decode('utf8'))
     except BaseException as e:
-        limitTime=1800
+        limitTime=2700
     return limitTime
 
 t=getLimitTime()
